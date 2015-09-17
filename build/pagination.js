5b51862c94b47131a7777bb9391ae1688adddecb
Russell Holmes <russellholmes@Russells-MacBook-Pro.local>
2015-09-17 22:53:39 +0100

    Initial start of pagination

--- build/pagination.js
+++ build/pagination.js
@@ -1,13 +1,29 @@
-var componentPaginationLeft = React.createClass({displayName: "componentPaginationLeft",
-  render : function() {
-    React.createElement("div", {class: "container-left"}, 
-      React.createElement("a", {href: "#"}, "Previous")
-    )
+// var CommentBox = React.createClass({
+//   render: function() {
+//     return (
+//       <div className="commentBox">
+//         Hello, world! I am a CommentBox.
+//       </div>
+//     );
+//   }
+// });
+
+var PaginationContainerLeft = React.createClass({displayName: "PaginationContainerLeft",
+  getInitialState: function() {
+   return {isVisible : (this.props.skip > 0 ? true : false)};
+  }
+, render : function() {
+    var linkTag = (this.state.isVisible ? '<a href="#">&lt; Previous</a>' : '');
+    return (
+      React.createElement("div", {className: "container-left"}, 
+        linkTag, 
+        React.createElement("a", {href: "#"}, "< Previous")
+      )
+    );
   }
 });
 
-
 React.render(
-  React.createElement(componentPaginationLeft, {}),
-  document.getElementById('tag2')
+  React.createElement(PaginationContainerLeft, {skip: "1"}),
+  document.getElementById('pagination')
 );
--- src/pagination.js
+++ src/pagination.js
@@ -1,13 +1,29 @@
-var componentPaginationLeft = React.createClass({
-  render : function() {
-    <div class='container-left'>
-      <a href='#'>Previous</a>
-    </div>
+// var CommentBox = React.createClass({
+//   render: function() {
+//     return (
+//       <div className="commentBox">
+//         Hello, world! I am a CommentBox.
+//       </div>
+//     );
+//   }
+// });
+
+var PaginationContainerLeft = React.createClass({
+  getInitialState: function() {
+   return {isVisible : (this.props.skip > 0 ? true : false)};
+  }
+, render : function() {
+    var linkTag = (this.state.isVisible ? '<a href="#">&lt; Previous</a>' : '');
+    return (
+      <div className="container-left">
+        {linkTag}
+        <a href="#">&lt; Previous</a>
+      </div>
+    );
   }
 });
 
-
 React.render(
-  React.createElement(componentPaginationLeft, {}),
-  document.getElementById('tag2')
+  <PaginationContainerLeft skip="1"/>,
+  document.getElementById('pagination')
 );
